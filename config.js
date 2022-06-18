3d00fa46fc492d031fea3fce86ddc46fc5e93b90
Frish2021-open <107548681+Frish2021-open@users.noreply.github.com>
2022-06-18 12:05:56 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+//电脑网站的配置文件
+//zh-cn
+var cn = "测试网站";
+var webtext = "你好";
+//EN-us
+var us = "text website";
+var usweb = "hello";
+//Jp-jp
+var jp = "テストサイト";
+var jpweb = "こんにちは";
+//Kr-Kr
+var kr = "테스트 사이트";
+var krweb = "안녕하십니까";
+//RU-ru
+var ru = "испытательный полигон";
+var ruweb = "Привет, товарищ";
